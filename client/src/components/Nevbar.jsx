008b847f79170c98a49ab3004827d3bcd1eefb06
Akshaj Mishra <akshajmishra3@gmail.com>
2025-04-03 20:52:16 +0530

    Initial commit

--- client/src/components/Nevbar.jsx
+++ client/src/components/Nevbar.jsx
@@ -6,17 +6,17 @@ export const Navbar = () => {
   const [user, setUser] = useState(false); // ignore
 
   return (
-    <div className="flex items-center justify-between py-4">
+    <div className='flex items-center justify-between py-4'>
       <Link to="/">
-        <img src={assets.logo} alt="Logo" className="w-28 sm:w-32 lg:w-40" />
+        <img src={assets.logo} alt="Logo" className='w-28 sm:w-32 lg:w-40' />
       </Link>
 
       <div>
         {user ? (
           <div></div>
         ) : (
-          <div className="flex items-center gap-2 sm:gap-5">
-            <button className=" bg-zinc-800 text-white px-7 py-2  sm:px-10 rounded-full hover:bg-zinc-700 transition" onClick={() => console.log("woopdi")} >
+          <div className='flex items-center gap-2 sm:gap-5'>
+            <button className=' bg-zinc-800 text-white px-7 py-2  sm:px-10 text-sm rounded-full'>
                login
             </button>
           </div>
