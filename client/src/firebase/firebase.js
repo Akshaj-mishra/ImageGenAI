70a24305c7bd9dfdf4fa3c45fcca4ffa6bca4baf
Akshaj Mishra <141712076+Akshaj-mishra@users.noreply.github.com>
2025-05-17 17:41:20 +0530

    Update firebase.js

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -1,19 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBF9dyuhxdz76jl6HkvYxXlXGvBDyG9H1o",
-  authDomain: "imagegrnai.firebaseapp.com",
-  projectId: "imagegrnai",
-  storageBucket: "imagegrnai.firebasestorage.app",
-  messagingSenderId: "885047916818",
-  appId: "1:885047916818:web:5a43dc76c9eced4cafdbcb",
-  measurementId: "G-PDXVHKP0TE"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId
 };
 
 // Initialize Firebase
